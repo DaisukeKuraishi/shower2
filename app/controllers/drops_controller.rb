86e4f73d2cedc473622b4c6369fd143458189ce8
DaisukeKuraishi <madgga@gmail.com>
2018-03-20 16:04:22 +0900

    一通りの機能実装、kaminari

--- app/controllers/drops_controller.rb
+++ app/controllers/drops_controller.rb
@@ -1,22 +1,52 @@
 class DropsController < ApplicationController
   def index
+    @drops = Drop.page(params[:page]).per(10)
   end
 
   def new
+    @drop = Drop.new
   end
 
   def create
+    @drop = Drop.new(drop_params)
+    if  @drop.save
+      redirect_to @drop
+    else
+      render :new
+    end
   end
 
   def edit
+    @drop = Drop.find(params[:id])
   end
 
   def update
+    @drop = Drop.find(params[:id])
+    if  @drop.update(drop_params)
+      redirect_to @drop
+    else
+      render :edit
+    end
   end
 
   def show
+     @drop = Drop.find(params[:id])
   end
 
   def destroy
+    @drop = Drop.find(params[:id])
+    @drop.destroy
+    redirect_to drops_path
+  end
+
+  private
+
+  def drop_params
+    params.require(:drop).permit(
+      :title,
+      :reason,
+      :p_key,
+      :sns
+      )
   end
 end
